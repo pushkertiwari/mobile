f354b5c1c03f635712cb2a66d1ce61153ca8b167
sonu654 <sonusanwal65@gmail.com>
2019-06-26 06:09:09 +0530

    UI Update

--- src/routes/Navigations.js
+++ src/routes/Navigations.js
@@ -1,23 +1,22 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 
 /***
  * Mobile Screens ree
  * start date 20-06-2019
  */
-import Home from '../containers/homeContainer';
-import Cloud from '../containers/cloudContainer';
-import Work from '../containers/workContainer';
-import Camera from '../containers/cameraContainer';
-import Analysis from '../containers/analysisContainer';
+import Home from "../containers/homeContainer";
+import Cloud from "../containers/cloudContainer";
+import Work from "../containers/workContainer";
+import Camera from "../containers/cameraContainer";
+import Analysis from "../containers/analysisContainer";
 
-
-export default Nagivations = createAppContainer(
-    createStackNavigator({ 
-        Work: { screen:Work},
-        Camera: {screen:Camera},
-        Home: { screen: Home },
-        Cloud: { screen: Cloud },
-        Analysis: { screen:Analysis},
-    })
-);
+export default (Nagivations = createAppContainer(
+  createStackNavigator({
+    Home: { screen: Home },
+    Work: { screen: Work },
+    Camera: { screen: Camera },
+    Cloud: { screen: Cloud },
+    Analysis: { screen: Analysis },
+  })
+));
